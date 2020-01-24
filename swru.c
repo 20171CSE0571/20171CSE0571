712f3ad058bd5246369d4e7526139d3621b66ac3
20171CSE0571 <60249236+20171CSE0571@users.noreply.github.com>
2020-01-24 10:54:44 +0530

    Add files via upload

---
+++ swru.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+      int a,b,res;
+      printf("enter the value of a and b\n");
+      scanf("%d%d",&a,&b);
+      printf("value of a before postincrement is %d\n",a);
+      printf("value of b before decrement is %d\n",b);
+      res=a+++b;
+      printf("result=%d\n",res);
+      printf("value of a after postincrement is %d\n",a);
+      printf("value of b after decrement is %d\n",b);
+}
